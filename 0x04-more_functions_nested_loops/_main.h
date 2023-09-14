dcbb8a349c7749ffef030285d48b0ec55c7c9afe
Praise-JORDAN <praiseegbuloni@gmail.com>
2023-09-14 16:21:16 +0100

    prototype

---
+++ 0x04-more_functions_nested_loops/_main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char );
+char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+
+#endif
